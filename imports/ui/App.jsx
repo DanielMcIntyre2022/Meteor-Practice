3e5fb9c677d546ee99c0c8750567791d6933de16
Daniel <daniel-mcintyre@hotmail.com>
2022-08-24 16:24:37 -0400

    form input setup

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '../api/TaskCollection';
+import { Task } from './Task.jsx';
+import { TaskForm } from './TaskForm';
 
-const tasks = [
-  {
-    id: 1,
-    text: 'First Task',
-  },
-  {
-    id: 2,
-    text: 'Second Task',
-  },
-  {
-    id: 3,
-    text: 'Third Task',
-  }
-]
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find({}).fetch());
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
-);
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+          <TaskForm/>
+      <ul>
+        { tasks.map(task => <Task key={ task._id } task={ task }/>) }
+      </ul>
+    </div>
+  );
+};
+
+  
